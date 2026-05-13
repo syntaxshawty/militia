43e677d9748fbd4b13bd1266ef6ca7af46990ee3
blueelle davidovich <med1198@gmail.com>
2026-05-13 11:56:15 -0700

    temp: updates contract address for testing

--- lib/web3/contract.ts
+++ lib/web3/contract.ts
@@ -3,7 +3,7 @@ import { sepolia } from "@/lib/web3/chains";
 import { MILITIA_ABI } from "@/lib/web3/abis/militia";
 
 export const CONTRACT_ADDRESSES: Partial<Record<number, Address>> = {
-  [sepolia.id]: "0x18DaF7F733763E99111e6Dbe8955d052015691c3",
+  [sepolia.id]: "0x52E582c0312112A4868e379e5EEBf93dd899E2cB",
 };
 
 export function getContractAddress(chainId: number): Address | undefined {
